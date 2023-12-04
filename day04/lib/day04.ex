00683267b901069e7a8885aa5ecb5e558b4d6727
Zach Peterson <zachpeterson13@icloud.com>
2023-12-04 13:56:50 -0600

    setup for day04

---
+++ day04/lib/day04.ex
@@ -0,0 +1,11 @@
+defmodule Day04 do
+  @spec part1(String.t()) :: integer()
+  def part1(_input) do
+    0
+  end
+
+  @spec part2(String.t()) :: integer()
+  def part2(_input) do
+    0
+  end
+end
