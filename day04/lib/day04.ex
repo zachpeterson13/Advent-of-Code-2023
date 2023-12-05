bbd5f1127aec94491acb4011c97d675106ec99af
Zach Peterson <zachpeterson13@icloud.com>
2023-12-04 18:52:46 -0600

    finished day04 part2, slow but works

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -1,5 +1,6 @@
 defmodule Day04 do
   @input File.read!("input.txt")
+
   @spec part1(String.t()) :: integer()
   def part1(input \\ @input) do
     input
@@ -30,8 +31,64 @@ defmodule Day04 do
 
   def check_for_winners(_, count), do: :math.pow(2, count - 1) |> floor()
 
+  @type card_map() :: %{pos_integer() => {[integer()], [integer()]}}
+
   @spec part2(String.t()) :: integer()
-  def part2(_input) do
-    0
+  def part2(input \\ @input) do
+    card_map =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_line/1)
+      |> Enum.reduce(%{}, fn map, acc -> Map.merge(map, acc) end)
+
+    q =
+      card_map
+      |> Map.keys()
+      |> :queue.from_list()
+
+    process_queue(card_map, q, :queue.len(q))
   end
+
+  @spec parse_line(String.t()) :: card_map()
+  def(parse_line(line)) do
+    [game | [winners | [numbers | []]]] =
+      line |> String.split([":", " | "], trim: true) |> Enum.map(&String.split/1)
+
+    game = List.last(game) |> String.to_integer()
+    winners = Enum.map(winners, &String.to_integer/1)
+    numbers = Enum.map(numbers, &String.to_integer/1)
+
+    %{game => {winners, numbers}}
+  end
+
+  @spec process_queue(card_map(), :queue.queue(), non_neg_integer()) :: integer()
+  def process_queue(_, {[], []}, count), do: count
+
+  def process_queue(map, q, count) do
+    {{:value, num}, q} = :queue.out(q)
+    winners = count_winners(map[num])
+
+    if winners == 0 do
+      process_queue(map, q, count + winners)
+    else
+      q =
+        (num + 1)..(num + winners)
+        |> Enum.reduce(q, fn n, acc -> :queue.in(n, acc) end)
+
+      process_queue(map, q, count + winners)
+    end
+  end
+
+  @spec count_winners({[integer()], [integer()]}, non_neg_integer()) :: integer()
+  def count_winners(nums, count \\ 0)
+
+  def count_winners({winners, [head | tail]}, count) do
+    if head in winners do
+      count_winners({winners, tail}, count + 1)
+    else
+      count_winners({winners, tail}, count)
+    end
+  end
+
+  def count_winners(_, count), do: count
 end
--- day04/test/day04_test.exs
+++ day04/test/day04_test.exs
@@ -2,7 +2,7 @@ defmodule Day04Test do
   use ExUnit.Case
   import Day04
 
-  test "test check_for_winners math" do
+  test "check_for_winners math" do
     result1 = check_for_winners({}, 4)
     result2 = check_for_winners({}, 0)
 
@@ -10,7 +10,13 @@ defmodule Day04Test do
     assert result2 == 0
   end
 
-  test "test part1" do
+  test "count_winners" do
+    result = count_winners({[41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]}, 0)
+
+    assert result == 4
+  end
+
+  test "part1" do
     input = """
     Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -23,5 +29,21 @@ defmodule Day04Test do
     result = part1(input)
 
     assert result == 13
+  end
+
+  # @tag :skip
+  test "part2" do
+    input = """
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """
+
+    result = part2(input)
+
+    assert result == 30
   end
 end
