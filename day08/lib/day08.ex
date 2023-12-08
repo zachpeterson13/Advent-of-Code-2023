c49ea916ed1e206c1516209dd573cbc2023a285a
Zach Peterson <zachpeterson13@icloud.com>
2023-12-08 13:16:05 -0600

    completed day08 part 2

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -50,7 +50,44 @@ defmodule Day08 do
     end
   end
 
-  def part2(input) do
-    input
+  def part2(input \\ @input) do
+    {inst_q, map} = input |> parse_input()
+
+    find_starts(map)
+    |> Enum.map(&process2(map, inst_q, &1))
+    # |> Task.async_stream(&process2(map, inst_q, &1))
+    # |> Enum.map(fn {:ok, val} -> val end)
+    |> Enum.reduce(&lcm/2)
   end
+
+  def find_starts(map) do
+    map
+    |> Map.keys()
+    |> Enum.filter(fn key -> match?(<<_, _, ?A>>, key) end)
+  end
+
+  def process2(map, inst_q, key, acc \\ 0)
+
+  def process2(_, _, <<_, _, ?Z>>, acc), do: acc
+
+  def process2(map, inst_q, key, acc) do
+    {left, right} = map[key]
+
+    {{:value, direction}, inst_q} = :queue.out(inst_q)
+
+    inst_q = :queue.in(direction, inst_q)
+
+    if direction == ?L do
+      process2(map, inst_q, left, acc + 1)
+    else
+      process2(map, inst_q, right, acc + 1)
+    end
+  end
+
+  def gcd(a, 0), do: a
+  def gcd(0, b), do: b
+  def gcd(a, b), do: gcd(b, rem(a, b))
+
+  def lcm(0, 0), do: 0
+  def lcm(a, b), do: (a * b / gcd(a, b)) |> floor()
 end
--- day08/test/day08_test.exs
+++ day08/test/day08_test.exs
@@ -33,4 +33,23 @@ defmodule Day08Test do
 
     assert result == 6
   end
+
+  test "part2" do
+    input = """
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+    """
+
+    result = part2(input)
+
+    assert result == 6
+  end
 end
