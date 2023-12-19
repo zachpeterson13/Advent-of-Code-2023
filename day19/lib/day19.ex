6e907d7b7f27161f2dc742ad941bb7936765e599
Zach Peterson <zachpeterson13@icloud.com>
2023-12-19 16:11:29 -0600

    completed day 19 part 2

--- day19/lib/day19.ex
+++ day19/lib/day19.ex
@@ -126,7 +126,79 @@ defmodule Day19 do
     |> Enum.sum()
   end
 
+  def parse_input2(input) do
+    {workflows, _} = input |> String.split("\n\n", trim: true) |> List.to_tuple()
+
+    workflows
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(%{"A" => [{"A"}], "R" => [{"R"}]}, fn wrk_str, acc ->
+      {name, rules} = String.split(wrk_str, ["{", "}"], trim: true) |> List.to_tuple()
+
+      rules =
+        rules
+        |> String.split(",", trim: true)
+        |> Enum.map(fn rule_str ->
+          rule_str |> String.split(":", trim: true) |> List.to_tuple()
+        end)
+
+      Map.merge(acc, %{name => rules})
+    end)
+  end
+
+  def apply_rules(workflows, %{x: x, m: m, a: a, s: s} = range, wf) do
+    rules = workflows[wf]
+
+    Enum.reduce(rules, [{range, wf}], fn
+      {rule_str, next}, [{range, _} | tail] ->
+        {category, rest} = String.split_at(rule_str, 1)
+        {sign, num} = String.split_at(rest, 1)
+        num = String.to_integer(num)
+        category = String.to_atom(category)
+
+        new_range =
+          case sign do
+            "<" ->
+              [
+                {%{range | category => num..range[category].last}, next},
+                {%{range | category => range[category].first..(num - 1)}, next}
+                | tail
+              ]
+
+            ">" ->
+              [
+                {%{range | category => range[category].first..num}, next},
+                {%{range | category => (num + 1)..range[category].last}, next}
+                | tail
+              ]
+          end
+
+        new_range
+
+      {next}, [{range, _} | tail] ->
+        [{range, next} | tail]
+    end)
+  end
+
+  def process2(workflows, ranges) do
+    if Enum.all?(ranges, fn {_, wf} -> wf == "A" or wf == "R" end) do
+      ranges
+    else
+      new_ranges = Enum.flat_map(ranges, fn {range, wf} -> apply_rules(workflows, range, wf) end)
+
+      process2(workflows, new_ranges)
+    end
+  end
+
   def part2(input \\ @input) do
-    input
+    workflows = input |> parse_input2()
+
+    start = [{%{x: 1..4000, m: 1..4000, a: 1..4000, s: 1..4000}, "in"}]
+
+    process2(workflows, start)
+    |> Enum.filter(fn {_, status} -> status == "A" end)
+    |> Enum.map(fn {%{x: x, m: m, a: a, s: s}, _} ->
+      Enum.count(x) * Enum.count(m) * Enum.count(a) * Enum.count(s)
+    end)
+    |> Enum.sum()
   end
 end
