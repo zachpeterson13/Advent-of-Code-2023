2b7937569e0e252c35f5bfb94e1f3613e2a8f138
Zach Peterson <zachpeterson13@icloud.com>
2023-12-20 13:14:14 -0600

    Day 20 setup

---
+++ day20/lib/day20.ex
@@ -0,0 +1,11 @@
+defmodule Day20 do
+  @input File.read!("input.txt")
+
+  def part1(input \\ @input) do
+    input
+  end
+
+  def part2(input \\ @input) do
+    input
+  end
+end
---
+++ day20/test/day20_test.exs
@@ -0,0 +1,32 @@
+defmodule Day20Test do
+  use ExUnit.Case
+  import Day20
+
+  test "part1 - 1" do
+    input = """
+    broadcaster -> a, b, c
+    %a -> b
+    %b -> c
+    %c -> inv
+    &inv -> a
+    """
+
+    result = part1(input)
+
+    assert result == 32_000_000
+  end
+
+  test "part1 - 2" do
+    input = """
+    broadcaster -> a
+    %a -> inv, con
+    &inv -> b
+    %b -> con
+    &con -> output
+    """
+
+    result = part1(input)
+
+    assert result == 11_687_500
+  end
+end
