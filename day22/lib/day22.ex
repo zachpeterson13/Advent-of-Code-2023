79fabac4379741f14bcb9bf8c09b716f7e1932e2
Zach Peterson <zachpeterson13@icloud.com>
2023-12-22 12:44:37 -0600

    day 22 setup

---
+++ day22/lib/day22.ex
@@ -0,0 +1,9 @@
+defmodule Day22 do
+  def part1(input) do
+    input
+  end
+
+  def part2(input) do
+    input
+  end
+end
---
+++ day22/test/day22_test.exs
@@ -0,0 +1,20 @@
+defmodule Day22Test do
+  use ExUnit.Case
+  import Day22
+
+  test "part1" do
+    input = """
+    1,0,1~1,2,1
+    0,0,2~2,0,2
+    0,2,3~2,2,3
+    0,0,4~0,2,4
+    2,0,5~2,2,5
+    0,1,6~2,1,6
+    1,1,8~1,1,9
+    """
+
+    result = part1(input)
+
+    result == 5
+  end
+end
